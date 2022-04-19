0fd871a21d3a188ff536e50bb0cb79f5484b39bf
Nikita Rogalenko <nikitalr@mail.ru>
2022-04-20 01:56:18 +0300

    ergast api data extractor

--- data-extractor/main.py
+++ data-extractor/main.py
@@ -1,7 +1,7 @@
 from ergast_api_extractor import process_f1_ergast_data
 
 if __name__ == '__main__':
-    # getting all available data from ergast developer api
+    # getting all available data from ergast developer api and saving it to required format
     process_f1_ergast_data("../f1_extracted_data/")
 
 
