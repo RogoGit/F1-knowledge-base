739102b046f4702a57b9c0bd966c5ff00934336d
Nikita Rogalenko <nikitalr@mail.ru>
2022-04-21 22:08:17 +0300

    saving cars and teams information from f1-technical.com to JSON

--- data-extractor/main.py
+++ data-extractor/main.py
@@ -1,7 +1,9 @@
 from ergast_api_extractor import process_f1_ergast_data
+from f1_technical_scrapper import get_f1_technical_data
 
 if __name__ == '__main__':
     # getting all available data from ergast developer api and saving it to required format
     process_f1_ergast_data("../f1_extracted_data/")
-
+    # getting cars and teams open data from f1-technical.com and saving it to required format
+    get_f1_technical_data("../f1_extracted_data/")
 
