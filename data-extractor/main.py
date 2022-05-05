622b484f6cad512d3dbd3c2eb0bfbc61a273f20c
Nikita Rogalenko <nikitalr@mail.ru>
2022-05-06 00:56:22 +0300

    extracting f1 books info

--- data-extractor/main.py
+++ data-extractor/main.py
@@ -1,6 +1,7 @@
 from ergast_api_extractor import process_f1_ergast_data
 from f1_technical_scrapper import get_f1_technical_data
 from f1_fansite_scrapper import get_f1_fansite_data
+from f1_books_extractor import get_f1_books_data
 
 
 if __name__ == '__main__':
@@ -10,4 +11,6 @@ if __name__ == '__main__':
     get_f1_technical_data("../f1_extracted_data/")
     # getting data from f1-fansite.com and saving it to required format
     get_f1_fansite_data("../f1_extracted_data/")
+    # getting f1 books data from goodreads and saving it to required format
+    get_f1_books_data("../f1_extracted_data/")
 
