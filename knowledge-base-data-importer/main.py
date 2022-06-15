f91d45f679793647f80e34489538bd23022bcf6a
Nikita Rogalenko <nikitalr@mail.ru>
2022-06-16 00:24:50 +0300

    little refactor, add comments to entites, add VoID, add pyLODE documentation, add populated ontology

--- knowledge-base-data-importer/main.py
+++ knowledge-base-data-importer/main.py
@@ -1,12 +1,16 @@
 from data_importer import fill_f1_graph
 from sparql_requester import run_queries
+from void_generator import generate_void
 
 
 ONTOLOGY_PATH = "../ontology.owl"
-RESULT_ONTOLOGY_PATH = "../ontology-with-individuals.owl"
+POPULATED_ONTOLOGY_PATH = "../ontology-with-individuals.owl"
 F1_EXTRACTED_DATA_PATH = "../f1_extracted_data"
+VOID_VOCAB_FILE = "../VoID.owl"
+ONTOLOGY_FORMAT = "turtle"
 
 
 if __name__ == '__main__':
-    fill_f1_graph(ONTOLOGY_PATH, "turtle", F1_EXTRACTED_DATA_PATH, RESULT_ONTOLOGY_PATH)
-    run_queries(RESULT_ONTOLOGY_PATH, "turtle")
+    fill_f1_graph(ONTOLOGY_PATH, ONTOLOGY_FORMAT, F1_EXTRACTED_DATA_PATH, POPULATED_ONTOLOGY_PATH)
+    generate_void(POPULATED_ONTOLOGY_PATH, ONTOLOGY_FORMAT, VOID_VOCAB_FILE)
+    run_queries(POPULATED_ONTOLOGY_PATH, ONTOLOGY_FORMAT)
